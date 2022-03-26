1cfbfe5fc6a55aac2d3aa481cf534ea423284f39
unknown <kumaravishek398@gmail.com>
2022-03-27 02:07:25 +0530

    modifiesd the privious java file

--- first.java
+++ first.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 class first
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		Scanner sc =new Scanner(System.in);
+
+		System.out.println("Enter your age");
+		int age =sc.nextInt();
+
+		if (age>20)
+		
+			System.out.println("you can marry");
+		else
+			System.out.println("control yourself");
+		
 	}
 }
